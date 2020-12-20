9d8d0895072e73d3338cc8b0131417b95c0bd527
Paul Woolcock <11843015+phw198@users.noreply.github.com>
2020-12-20 14:10:33 +0000

    Don't mask GetSettings() error with ApplicationException. #886

--- src/OutlookGoogleCalendarSync/GoogleOgcs/GoogleCalendar.cs
+++ src/OutlookGoogleCalendarSync/GoogleOgcs/GoogleCalendar.cs
@@ -1348,7 +1348,7 @@ namespace OutlookGoogleCalendarSync.GoogleOgcs {
                 this.UTCoffset = TimezoneDB.GetUtcOffset(setting.Value);
             } catch (System.Exception ex) {
                 OGCSexception.Analyse("Not able to retrieve Google calendar's global timezone", ex);
-                throw new System.ApplicationException("Unable to retrieve Google calendar's global timezone.", ex);
+                throw;
             }
             getCalendarSettings();
         }
@@ -1362,8 +1362,8 @@ namespace OutlookGoogleCalendarSync.GoogleOgcs {
                 else
                     this.MinDefaultReminder = cal.DefaultReminders.Where(x => x.Method.Equals("popup")).OrderBy(x => x.Minutes.Value).First().Minutes.Value;
             } catch (System.Exception ex) {
-                OGCSexception.Analyse("Failed to get calendar settings.", ex);
-                throw new System.ApplicationException("Unable to retrieve Google calendar's reminder settings.", ex);
+                OGCSexception.Analyse("Failed to get calendar's reminder settings.", ex);
+                throw;
             }
         }
 
