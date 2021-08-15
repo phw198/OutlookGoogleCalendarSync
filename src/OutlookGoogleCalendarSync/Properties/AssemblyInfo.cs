21aaa17a6343d4b316d30dff187f6ac2a0603225
Paul Woolcock <11843015+phw198@users.noreply.github.com>
2021-08-15 15:46:46 +0100

    log4net updated to v2.0.12 Newtonsoft.Json updated to v13.0 Google.Calendar.V3 APIs updated to v1.54.0
    Final versions targeting .Net 4.5:-
    Google.Cloud.Logging.V2 updated to v2.3
    Google.Cloud.Logging.Log4Net updated to v2.5
    NodaTime updated to v2.4.13
    SharpCompress updated to v0.24

--- src/OutlookGoogleCalendarSync/Properties/AssemblyInfo.cs
+++ src/OutlookGoogleCalendarSync/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("2.8.*")]
-[assembly: AssemblyFileVersion("2.8.7.0")]
+[assembly: AssemblyVersion("2.9.*")]
+[assembly: AssemblyFileVersion("2.9.1.0")]
 
 [assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
