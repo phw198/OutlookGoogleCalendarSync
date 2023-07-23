15243f642058f37b7dd03bcb933293d86eb2c00c
Paul Woolcock <11843015+phw198@users.noreply.github.com>
2023-07-23 15:39:32 +0100

    Update "G"oogle character on icon Icon update on each form

--- src/OutlookGoogleCalendarSync/Properties/Resources.Designer.cs
+++ src/OutlookGoogleCalendarSync/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace OutlookGoogleCalendarSync.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -127,6 +127,16 @@ namespace OutlookGoogleCalendarSync.Properties {
             get {
                 object obj = ResourceManager.GetObject("github", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon icon {
+            get {
+                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
             }
         }
         
