c60f82e69d993b35603eb71aa4306ca1da8ed3b0
Paul Woolcock <11843015+phw198@users.noreply.github.com>
2024-06-22 14:55:16 +0100

    Simplified building Telemetry object.
    (cherry picked from commit 3ab1b390df805d6136f0dd11690378d117436539)

--- src/OutlookGoogleCalendarSync/Program.cs
+++ src/OutlookGoogleCalendarSync/Program.cs
@@ -521,8 +521,9 @@ namespace OutlookGoogleCalendarSync {
                     Helper.OpenBrowser(OgcsWebsite + releaseNotesUrl);
                     if (isSquirrelInstall) {
                         Telemetry.Send(Analytics.Category.squirrel, Analytics.Action.upgrade, "from=" + settingsVersion + ";to=" + Application.ProductVersion);
-                        Telemetry.GA4Event.Event squirrelGaEv = new(Telemetry.GA4Event.Event.Name.squirrel);
-                        squirrelGaEv.AddParameter(GA4.Squirrel.upgraded_from, settingsVersion);
+                        new Telemetry.GA4Event.Event(Telemetry.GA4Event.Event.Name.squirrel)
+                            .AddParameter(GA4.Squirrel.upgraded_from, settingsVersion)
+                            .Send();
                     }
                 }
             }
@@ -568,12 +569,12 @@ namespace OutlookGoogleCalendarSync {
             try {
                 Telemetry.Send(Analytics.Category.ogcs, Analytics.Action.donate, source);
                 Telemetry.Send(Analytics.Category.ogcs, Analytics.Action.donate, Application.ProductVersion);
-                
-                Telemetry.GA4Event.Event donateGa4Ev = new(Telemetry.GA4Event.Event.Name.donate);
-                donateGa4Ev.AddParameter("source", source);
-                donateGa4Ev.AddParameter(GA4.General.sync_count, Settings.Instance.CompletedSyncs);
-                donateGa4Ev.AddParameter("account_present", !String.IsNullOrEmpty(Settings.Instance.GaccountEmail));
-                donateGa4Ev.Send();
+
+                new Telemetry.GA4Event.Event(Telemetry.GA4Event.Event.Name.donate)
+                    .AddParameter("source", source)
+                    .AddParameter(GA4.General.sync_count, Settings.Instance.CompletedSyncs)
+                    .AddParameter("account_present", !String.IsNullOrEmpty(Settings.Instance.GaccountEmail))
+                    .Send();
 
             } finally {
                 Helper.OpenBrowser("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=44DUQ7UT6WE2C&item_name=Outlook Google Calendar Sync from " + Settings.Instance.GaccountEmail);
