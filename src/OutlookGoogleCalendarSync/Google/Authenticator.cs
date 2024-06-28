9f4dc41aed2e42d919f959a6f848a5809794c1ad
Paul Woolcock <11843015+phw198@users.noreply.github.com>
2024-06-28 01:18:55 +0100

    Check for registered donation to hide splash screen.

--- src/OutlookGoogleCalendarSync/Google/Authenticator.cs
+++ src/OutlookGoogleCalendarSync/Google/Authenticator.cs
@@ -317,9 +317,17 @@ namespace OutlookGoogleCalendarSync.Google {
                 userDonationCheck();
                 checkedOgcsUserStatus = true;
 
-                if (Settings.Instance.UserIsBenefactor() && Settings.Instance.HideSplashScreen == null) {
-                    Boolean hideSplash = Ogcs.Extensions.MessageBox.Show("Thank you for your support of OGCS!\r\nWould you like the splash screen to be hidden from now on?", "Hide Splash Screen?",
-                        MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes;
+                Boolean? hideSplash = null;
+                if (Settings.Instance.UserIsBenefactor()) {
+                    if (Settings.Instance.HideSplashScreen == null) {
+                        hideSplash = Ogcs.Extensions.MessageBox.Show("Thank you for your support of OGCS!\r\nWould you like the splash screen to be hidden from now on?", "Hide Splash Screen?",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes;
+                    }
+                } else if (Settings.Instance.HideSplashScreen == true) {
+                    log.Fine("Splash screen was set to hide without a donation being registered.");
+                    hideSplash = false;
+                }
+                if (hideSplash != null) {
                     Forms.Main.Instance.SetControlPropertyThreadSafe(Forms.Main.Instance.cbHideSplash, "Checked", hideSplash);
                     Settings.Instance.HideSplashScreen = hideSplash;
                 }
