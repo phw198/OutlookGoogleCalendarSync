be8105e392f3dfe9ba8892f1831b1a130d771136
Paul Woolcock <11843015+phw198@users.noreply.github.com>
2024-11-30 11:38:40 +0000

    Extra logging for ai.LastModifiedDateTime #1978

--- src/OutlookGoogleCalendarSync/Google.Graph/GoogleCalendar.cs
+++ src/OutlookGoogleCalendarSync/Google.Graph/GoogleCalendar.cs
@@ -786,16 +786,21 @@ namespace OutlookGoogleCalendarSync.Google.Graph {
                 log.Fine("Back processing Event affected by attendee API limit.");
             } else {
                 if (!(Sync.Engine.Instance.ManualForceCompare || forceCompare)) { //Needed if the exception has just been created, but now needs updating
-                    if (profile.SyncDirection.Id != Sync.Direction.Bidirectional.Id) {
-                        if (ev.Updated > ai.LastModifiedDateTime?.ToLocalTime())
-                            return null;
-                    } else {
-                        if (Outlook.Graph.CustomProperty.GetOGCSlastModified(ai).AddSeconds(5) >= ai.LastModifiedDateTime?.ToLocalTime()) {
-                            log.Fine("Outlook last modified by OGCS.");
-                            return null;
+                    try {
+                        if (profile.SyncDirection.Id != Sync.Direction.Bidirectional.Id) {
+                            if (ev.Updated > ai.LastModifiedDateTime?.ToLocalTime())
+                                return null;
+                        } else {
+                            if (Outlook.Graph.CustomProperty.GetOGCSlastModified(ai).AddSeconds(5) >= ai.LastModifiedDateTime?.ToLocalTime()) {
+                                log.Fine("Outlook last modified by OGCS.");
+                                return null;
+                            }
+                            if (ev.Updated > ai.LastModifiedDateTime?.ToLocalTime())
+                                return null;
                         }
-                        if (ev.Updated > ai.LastModifiedDateTime?.ToLocalTime())
-                            return null;
+                    } catch (System.Exception ex) {
+                        log.Debug("ai.LastModifiedDateTime: " + ai.LastModifiedDateTime?.ToString());
+                        throw;
                     }
                 }
             }
