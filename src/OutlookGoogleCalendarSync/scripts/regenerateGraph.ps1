7cbcf9e84bbed150d1bd4ecf1b553432afc4b90c
Paul Woolcock <11843015+phw198@users.noreply.github.com>
2026-06-09 17:32:48 -0300

    Production Graph API definition as of 9-Jun-2026 Repoint Graph generation to local definition file.

--- src/OutlookGoogleCalendarSync/scripts/regenerateGraph.ps1
+++ src/OutlookGoogleCalendarSync/scripts/regenerateGraph.ps1
@@ -1,7 +1,10 @@
 Write-Host "Regenerating Custom Microsoft Graph Client via Kiota..." -ForegroundColor Cyan
 
+# Latest verson of the properly mastered API YAML:-
+# https://raw.githubusercontent.com/microsoftgraph/msgraph-metadata/master/openapi/v1.0/openapi.yaml
+
 .\kiota.exe generate `
-  -d "https://raw.githubusercontent.com/microsoftgraph/msgraph-metadata/master/openapi/v1.0/openapi.yaml" `
+  -d "openapi.yaml" `
   -l CSharp `
   -c GraphServiceClient `
   -n OutlookGoogleCalendarSync.Outlook.Graph.CustomClient `
